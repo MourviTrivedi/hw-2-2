9394851b9080b6850365bdb35752181c159cc7ab
mourvi trivedi <mt67@afsconnect1.njit.edu>
2017-02-20 16:28:36 -0500

    updated php

--- hw-2-2/display_results.php
+++ hw-2-2/display_results.php
@@ -67,7 +67,7 @@
 
         <label>Future Value:</label>
         <span><?php echo $future_value_f; ?></span><br>
-	<p>This calculation was done on <?php echo date('m/'d/Y'); ?>.</p>
+	<p>This calculation was done on <?php echo date('m/d/Y'); ?>.</p>
     </main>
 </body>
 </html>
